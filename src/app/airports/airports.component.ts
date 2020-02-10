27178eeccbfd206b16af972e21e98e9ca93100dc
HaniehAmini <40342894+aminihanieh@users.noreply.github.com>
2020-02-10 16:57:34 +0330

    organize the folders

--- src/app/airports/airports.component.ts
+++ src/app/airports/airports.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {DataService} from '../data.service'
-import { DatabaseService } from '../database.service';
+import {DataService} from '../services/data.service'
+import { DatabaseService } from '../services/database.service';
 @Component({
   selector: 'app-airports',
   templateUrl: './airports.component.html',
