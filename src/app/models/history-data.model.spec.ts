4a2585567d160be0fb8a96701426926b72dad770
HaniehAmini <40342894+aminihanieh@users.noreply.github.com>
2020-02-22 15:35:54 +0330

    Delete history-data.model.spec.ts

--- src/app/models/history-data.model.spec.ts
+++ src/app/models/history-data.model.spec.ts
@@ -1,7 +1,1 @@
-import { HistoryData } from './history-data.model';
-
-describe('HistoryData', () => {
-  it('should create an instance', () => {
-    expect(new HistoryData()).toBeTruthy();
-  });
 });
